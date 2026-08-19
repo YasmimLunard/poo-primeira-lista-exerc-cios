e0a3a3fa2259161bca8eee91b96182761501e6d3
Yasmim Moreira <yasmimmoreiralunardi@gmail.com>
2026-08-19 20:22:33 -0300

    Codigo pedido na atividade

--- src/main/java/com/mycompany/atividade/rodrigo/HelloWorld.java
+++ src/main/java/com/mycompany/atividade/rodrigo/HelloWorld.java
@@ -1,16 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.atividade.rodrigo;
 
 /**
  *
  * @author aluno
  */
-public class AtividadeRodrigo {
-
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Yasmim Moreira Lunardi - Mue primeiro codigo Java!");
     }
+    
+    
 }
